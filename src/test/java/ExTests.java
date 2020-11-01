47a433d8727be3a6780896138bc81820dfc34e0e
azolayza <azovcevae@gmail.com>
2020-11-01 18:34:26 +0700

    Ex7
    Add new  device capability  for start test in portrait orientation

--- src/test/java/ExTests.java
+++ src/test/java/ExTests.java
@@ -28,6 +28,7 @@ public class ExTests {
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","/Users/liza/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("orientation", "PORTRAIT");
 
         driver = new AndroidDriver(new URL("http:127.0.0.1:4723/wd/hub"), capabilities);
     }
